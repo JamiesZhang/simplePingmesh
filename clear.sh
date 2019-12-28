7ada4111f8ef6fb4132ee121f80ee2c2f012b28d
JamiesZhang <zhangjiahua@ict.ac.cn>
2019-12-28 23:29:24 +0800

    debug 不要乱用&后台运行

--- clear.sh
+++ clear.sh
@@ -1,15 +1,17 @@
 #! /bin/bash
-
+username=$USER
+echo "用户名是：$username"
 #清除服务器端的文件夹重建
 clearway(){
 	{
-		#echo $1,$2,$3
-		echo "清除$1服务器上的残留文档" # 就不新建了，用的时候新建
+		#echo $1,$2
+		echo "清除 $1 服务器上的残留文档" # 就不新建了，用的时候新建
 		#$1，is the server ip
 		#$2，is the start of the 目录
-		scp everyclear.sh $USER@$1:~/pingmeshtest #需要打印一次密码
+		scp everyclear.sh zhangjiahua@$1:~/pingmeshtest/ #需要打印一次密码
 		#sleep 2
-		ssh -t -t $RUSE@$1 "cd ~/pingmeshtest/; bash everyclear.sh $2 $3 " #需要打印一次密码
+		#echo "copy ok $2"
+		ssh -t  $USER@$1 "cd ~/pingmeshtest/; bash everyclear.sh $2"
 	} 
 
 }
@@ -36,17 +38,15 @@ do
 	((mmm++))
 done
 #mmm代表有多少个Ping
-echo "共有$mmm个服务器"
+echo "共有$mmm个服务器需要清理"
 
-count=1
+#count=1
 for i in ${ARRAY[*]}
 do
 	{
-		clearway $i $count $mmm
-	} &
-	#sleep 3 &
-	#wait $!
-	count=$[ $count + 1 ] # ????这里可能有点问题，不一定能把残留文件删全，看看再说吧
+		echo "开始清理 $i "
+		clearway $i $mmm
+	} # 这里千万不要乱用 & 后台执行，不然会发生奇怪的错误!!!!!!!!!!!
 done
 
 # {
@@ -55,6 +55,6 @@ done
 # sleep 1 &
 # wait $!
 
-{
-	killall -u $USER
-} & 
+#{
+#	killall -u $USER
+#} & 
--- everyclear.sh
+++ everyclear.sh
@@ -1,12 +1,14 @@
 #clearforeach
-
+hname=$(hostname)
 echo "开始清除空间"
 # rm -rf *
-for ((i=$1; i<$[$2+$1+1]; i ++))  
+mm=$(($1-1))
+tt=$(($1*$mm))
+for((i=1;i<=$tt;i++)); # 这里缺了等号“=”就不能正常运行了，就进不去循环了
 do  
-	echo $i;
+	echo “rm directory $i”;
 	rm -rf $i;
 	# mkdir $i;
 done  
-echo "清除完成，退出."
-killall -u $USER
+echo "清除完成，退出 $hname."
+#killall -u $USER   # 这个等之后看看有没有必要因为“port已经被使用的问题”加上这个killall，这个有点强劲
